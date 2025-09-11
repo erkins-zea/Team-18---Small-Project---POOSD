00f244fb06aac74974dd0be72b000603f49c65b1
Nathan <146783884+Corells@users.noreply.github.com>
2025-09-10 20:47:52 -0400

    add doLogin function and login-result span

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -5,3 +5,56 @@ let userId = 0;
 let firstName = "";
 let lastName = "";
 const ids = [];
+
+function doLogin() {
+    userId = 0;
+    firstName = "";
+    lastName = "";
+
+    let login = document.getElementById("login-user").value;
+    let password = document.getElementById("login-password").value;
+
+    // TODO: Declare md5 hash here
+
+    if (!validLoginForm(login, password)) {
+        document.getElementById("login-result").innerHTML = "Invalid username or password";
+        return;
+    }
+
+    document.getElementById("login-result").innerHTML = "";
+
+    let temp = {
+        login : login,
+        password : password, // Remember to replace with hash
+    };
+
+    let jsonPayload = JSON.stringify(temp);
+    let url = urlBase + "/Login." + extension; // Remember to rename if .php filename is different
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+        xhr.onreadystatechange = () => {
+            if (this.readyState == 4 && this.status == 200) {
+                let jsonObject = JSON.parse(xhr.responseText);
+                userId = jsonObject.id;
+
+                if (userId < 1) {
+                    document.getElementById("login-result").innerHTML = "User/Password combination incorrect";
+                    return;
+                }
+
+                firstName = jsonObject.firstName;
+                lastName = jsonObject.lastName;
+
+                saveCookie();
+
+                window.location.href = "contacts.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    } catch (error) {
+        document.getElementById("loginResult").innerHTML = error.message;
+    }
+}
