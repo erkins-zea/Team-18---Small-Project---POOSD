bea3b33632a9b929748f1c71c7105de4105dcaf7
Nathan <146783884+Corells@users.noreply.github.com>
2025-09-10 21:02:09 -0400

    add readCookie function and change welcome heading
    also reformatted using backtick strings

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -63,5 +63,28 @@ function saveCookie() {
     let minutes = 20;
     let date = new Date();
     date.setTime(date.getTime + (minutes * 60 * 1000));
-    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+    document.cookie = `firstName=${firstName},lastName=${lastName},userId=${userId};expires=${date.toGMTString()}`;
+}
+
+function readCookie() {
+    userId = -1;
+    let data = document.cookie;
+    let splits = data.split(',')
+
+    for (let i = 0; i < splits.length; i++) {
+        let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+
+		if (tokens[0] == "firstName")
+			firstName = tokens[1];
+		else if (tokens[0] == "lastName")
+			lastName = tokens[1];
+		else if (tokens[0] == "userId")
+			userId = parseInt(tokens[1].trim());
+    }
+
+    if (userId < 0)
+        window.location.href = "index.html"
+    else
+        document.getElementById("user-name").innerHTML = `Welcome, ${firstName} ${lastName}!`;
 }
