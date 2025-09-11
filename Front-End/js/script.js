b474211e96b40871740f85a3fe8272b2211da77a
Nathan <146783884+Corells@users.noreply.github.com>
2025-09-10 21:04:14 -0400

    add doLogout function

--- Front-End/js/script.js
+++ Front-End/js/script.js
@@ -88,3 +88,11 @@ function readCookie() {
     else
         document.getElementById("user-name").innerHTML = `Welcome, ${firstName} ${lastName}!`;
 }
+
+function doLogout() {
+    userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
